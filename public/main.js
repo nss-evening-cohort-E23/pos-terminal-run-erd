7e1cdf7133c1c664349fc470dd7fc817754aa70a
Michael Brantley <brantleyrecs@gmail.com>
2023-06-12 19:32:00 -0500

    stuff

--- public/main.js
+++ public/main.js
@@ -1,17 +1,10 @@
 // USE WITH FIREBASE AUTH
-// import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
+import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
 const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
-  `;
+  document.querySelector('#app').innerHTML = ''
   console.warn('YOU ARE UP AND RUNNING!');
 
   document
