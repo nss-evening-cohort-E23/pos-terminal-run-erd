061b4b9faa02c87f239dffb042bf341e8e0a7edd
datcog1111 <h9tuck@gmail.com>
2023-06-13 20:47:49 -0500

    dom events

--- utils/startApp.js
+++ utils/startApp.js
@@ -2,10 +2,12 @@
 import domBuilder from '../components/shared/domBuilder';
 import landingPage from '../pages/landingPage';
 // import { viewOrders } from '../pages/orders';
+import domEvents from '../components/events/domEvents';
 
 const startApp = () => {
   domBuilder();
   landingPage();
+  domEvents();
 
   // getOrder().then((cards) => viewOrders(cards));
 };
