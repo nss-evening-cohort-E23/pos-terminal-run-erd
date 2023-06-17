c6d523feb6628af07324f275b21f744e02c7815e
Michael Brantley <brantleyrecs@gmail.com>
2023-06-17 12:11:23 -0500

    added orderDetails page

--- components/shared/domBuilder.js
+++ components/shared/domBuilder.js
@@ -6,6 +6,10 @@ const domBuilder = () => {
       <div id="navigation"></div>
       <div id="all-orders"></div>
       <div id="order-page"></div>
+      <div>
+        <div id="order-total"></div>
+        <div id="order-details"></div>
+      </div>
       <div id="close-order"></div>
       <div id="landing-area"></div>
       <div id="form-area"></div>
